65a0f87ddfb410f51120d26094f8361ff28731ff
Timothy Ng <timnjj@gmail.com>
2022-12-18 17:36:36 -0800

    feat: use INPUT_REPO-TOKEN

--- github.go
+++ github.go
@@ -10,7 +10,7 @@ import (
 func GithubClient() *github.Client {
 	ctx := context.Background()
 	ts := oauth2.StaticTokenSource(
-		&oauth2.Token{AccessToken: GetRequiredEnv("GITHUB_TOKEN")},
+		&oauth2.Token{AccessToken: GetRequiredEnv("INPUT_REPO-TOKEN")},
 	)
 	tc := oauth2.NewClient(ctx, ts)
 	client := github.NewClient(tc)
