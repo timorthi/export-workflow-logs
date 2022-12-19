8332e6e72102e97934753d3a8f5b7ea9fb1ee7b3
Timothy Ng <timnjj@gmail.com>
2022-12-18 17:27:37 -0800

    feat: add constants

---
+++ constants.go
@@ -0,0 +1,8 @@
+package main
+
+const (
+	// GitHub Actions default env vars reference: https://docs.github.com/en/actions/learn-github-actions/environment-variables#default-environment-variables
+	envVarRepoOwner     string = "GITHUB_REPOSITORY_OWNER"
+	envVarRepoFullName  string = "GITHUB_REPOSITORY"
+	envVarWorkflowRunID string = "GITHUB_RUN_ID"
+)
