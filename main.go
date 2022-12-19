0a272d8a480ef8a640664c8f2da16edbd7bd0573
Timothy Ng <timnjj@gmail.com>
2022-12-18 17:27:57 -0800

    feat: add basic GetWorkflowRunLogs call

--- main.go
+++ main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"log"
+	"strconv"
+	"strings"
+)
 
 func main() {
-	fmt.Println("Hello world!")
+	log.Println("Hello world!")
+	client := GithubClient()
+
+	runIDString := GetRequiredEnv(envVarWorkflowRunID)
+	runID, parseErr := strconv.ParseInt(runIDString, 10, 64)
+	if parseErr != nil {
+		log.Fatalf("Could not convert runID '%s' to int64", runIDString)
+	}
+
+	url, response, err := client.Actions.GetWorkflowRunLogs(
+		context.Background(),
+		GetRequiredEnv(envVarRepoOwner),
+		strings.Split(GetRequiredEnv(envVarRepoFullName), "/")[1],
+		runID,
+		true,
+	)
+
+	log.Println(url)
+	log.Println(response)
+	log.Println(err)
 }
