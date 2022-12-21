24bd52c1dc3ec8e41aa4ee99f09eb699ad8768a1
Timothy Ng <timnjj@gmail.com>
2022-12-21 12:35:55 -0800

    feat: add run-id as input

--- constants.go
+++ constants.go
@@ -4,6 +4,7 @@ const (
 	// GitHub Actions default env vars reference: https://docs.github.com/en/actions/learn-github-actions/environment-variables#default-environment-variables
 	envVarRepoOwner       string = "GITHUB_REPOSITORY_OWNER"
 	envVarRepoFullName    string = "GITHUB_REPOSITORY"
-	envVarWorkflowRunID   string = "GITHUB_RUN_ID"
 	envVarGitHubWorkspace string = "GITHUB_WORKSPACE"
+	envVarRepoToken       string = "INPUT_REPO-TOKEN"
+	envVarWorkflowRunID   string = "INPUT_RUN-ID"
 )
