0a4741d4991995c8743140d4ad9cbfd98fe39ed4
Timothy Ng <timnjj@gmail.com>
2022-12-22 17:56:31 -0800

    feat: add inputs for aws s3

--- constants.go
+++ constants.go
@@ -14,4 +14,10 @@ const (
 var (
 	inputRepoTokenPtr     *string = flag.String("repo-token", "", "GITHUB_TOKEN or a Personal Access Token")
 	inputWorkflowRunIDPtr *int64  = flag.Int64("run-id", 0, "GitHub Actions Workflow Run ID")
+	inputDestination      *string = flag.String("destination", "", "The service to export workflow logs to")
+
+	inputAWSAccessKeyID     *string = flag.String("aws-access-key-id", "", "AWS Access Key ID")
+	inputAWSSecretAccessKey *string = flag.String("aws-secret-access-key", "", "AWS Secret Access Key")
+	inputS3BucketName       *string = flag.String("s3-bucket-name", "", "S3 bucket name")
+	inputS3Key              *string = flag.String("s3-key", "", "S3 key")
 )
