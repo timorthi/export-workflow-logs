8b7aeae367da0e11a75e0978223120b215e39793
Timothy Ng <timnjj@gmail.com>
2022-12-25 14:02:42 -0800

    test: add TestSaveToS3

---
+++ s3_test.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"context"
+	"io"
+	"os"
+	"strings"
+	"testing"
+
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+)
+
+type mockPutObjectAPI func(ctx context.Context, params *s3.PutObjectInput, optFns ...func(*s3.Options)) (*s3.PutObjectOutput, error)
+
+func (m mockPutObjectAPI) PutObject(ctx context.Context, params *s3.PutObjectInput, optFns ...func(*s3.Options)) (*s3.PutObjectOutput, error) {
+	return m(ctx, params, optFns...)
+}
+
+func TestSaveToS3(t *testing.T) {
+	ctx := context.Background()
+	tmpDir := t.TempDir()
+	tmpFile, err := os.CreateTemp(tmpDir, "tmpFile")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer tmpFile.Close()
+	testContents := "hello world!"
+	tmpFile.WriteString(testContents)
+
+	testBucket := "testBucketName"
+	testKey := "some/key/to/logs.zip"
+
+	testAPI := mockPutObjectAPI(func(ctx context.Context, params *s3.PutObjectInput, optFns ...func(*s3.Options)) (*s3.PutObjectOutput, error) {
+		t.Helper()
+		if *params.Key != testKey {
+			t.Fatalf("expected supplied key to be %s but got %s", testKey, *params.Key)
+		}
+		if *params.Bucket != testBucket {
+			t.Fatalf("expected supplied bucket to be %s but got %s", testBucket, *params.Bucket)
+		}
+
+		buf := new(strings.Builder)
+		io.Copy(buf, params.Body)
+		if buf.String() != testContents {
+			t.Fatalf("unexpected file contents: '%s'", buf.String())
+		}
+
+		return &s3.PutObjectOutput{}, nil
+	})
+
+	err = saveToS3(ctx, testAPI, testBucket, testKey, tmpFile.Name())
+	if err != nil {
+		t.Fatal(err)
+	}
+}
