160dfe2d69ea4a3a29f903e45e98612ac899a207
Timothy Ng <timnjj@gmail.com>
2022-12-25 12:07:24 -0800

    test: add TestDownloadFileByURL

--- util_test.go
+++ util_test.go
@@ -1,6 +1,37 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"strings"
+	"testing"
+)
+
+func TestDownloadFileByURL(t *testing.T) {
+	t.Setenv(envVarGitHubWorkspace, t.TempDir())
+	testFileContents := "someFileContents"
+
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, testFileContents)
+	}))
+	defer ts.Close()
+
+	pathToSavedFile, err := downloadFileByURL(ts.URL)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	contents, err := os.ReadFile(pathToSavedFile)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if strings.TrimSpace(string(contents)) != testFileContents {
+		t.Fatal("File contents did not match expected test file contents")
+	}
+}
 
 func TestGetRequiredEnv(t *testing.T) {
 	testCases := []struct {
